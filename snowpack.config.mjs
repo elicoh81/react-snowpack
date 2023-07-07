d9a81971f43640333cd007fa04bdde16b17c57c1
Eli Cohen <eli.cohen.ws@gmail.com>
2023-07-07 16:23:33 +0300

    initial

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,10 +1,25 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    /* ... */
+    // directory name: 'build directory'\
+    public: '/',
+    src: '/dist',
   },
   plugins: [
-    /* ... */
+    '@snowpack/plugin-react-refresh',
+    [
+      '@snowpack/plugin-typescript',
+      {
+        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
+        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
+      },
+    ],
+    [
+      '@snowpack/plugin-webpack',
+      {
+        /* see "Plugin Options" below */
+      },
+    ],
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
